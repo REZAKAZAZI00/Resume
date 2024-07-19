3049240a325090d44f01d7681e1e7c9b29a1bdaa
rezakazazy8 <rezakazazy8@yahoo.com>
2024-07-20 00:47:41 +0330

    global using

--- Resume.Core/UsingGlobal.cs
+++ Resume.Core/UsingGlobal.cs
@@ -1,6 +1,13 @@
-﻿global using System;
+﻿global using Resume.Core.DTOs.User;
+global using System;
 global using System.Collections.Generic;
 global using System.Linq;
 global using System.Text;
 global using System.Threading.Tasks;
 global using System.Security.Cryptography;
+global using Resume.DataLayer.Context;
+global using Resume.DataLayer.Entities.User;
+global using System.ComponentModel.DataAnnotations;
+global using Resume.Core.Services.InterFace;
+global using Microsoft.Extensions.Logging;
+global using Resume.Core.Security;
--- Resume/UsingGlobal.cs
+++ Resume/UsingGlobal.cs
@@ -1,2 +1,8 @@
 ﻿global using Microsoft.AspNetCore.Mvc;
 global using System.Diagnostics;
+global using Resume.Core.Services;
+global using Resume.Core.Services.InterFace;
+global using Microsoft.EntityFrameworkCore;
+global using Resume.DataLayer.Context;
+global using Resume.Web.Configuration;
+global using Serilog;
