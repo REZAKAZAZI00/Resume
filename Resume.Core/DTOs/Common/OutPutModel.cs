312c6cdb3091a135798fbd9cfafb98dc47061c6c
rezakazazy8 <rezakazazy8@yahoo.com>
2024-07-20 16:06:46 +0330

    Set outpoutmodel

---
+++ Resume.Core/DTOs/Common/OutPutModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Resume.Core.DTOs.Common;
+
+public class OutPutModel<T>
+{
+    public T? Result { get; set; }
+    public int StatusCode { get; set; }
+    public string? Message { get; set; }
+}
