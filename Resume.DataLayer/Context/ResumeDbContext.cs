ea39197dd8e9431e069f8c294c05eb21e7804585
rezakazazy8 <rezakazazy8@yahoo.com>
2024-07-20 21:39:22 +0330

    add migration

--- Resume.DataLayer/Context/ResumeDbContext.cs
+++ Resume.DataLayer/Context/ResumeDbContext.cs
@@ -1,12 +1,12 @@
 ﻿namespace Resume.DataLayer.Context
 {
-    public class ResumeDbContext:DbContext
+    public class ResumeDbContext : DbContext
     {
         #region Constructor
 
-        public ResumeDbContext(DbContextOptions<ResumeDbContext> options):base(options)
+        public ResumeDbContext(DbContextOptions<ResumeDbContext> options) : base(options)
         {
-                
+
         }
         #endregion
 
@@ -16,6 +16,49 @@
         #endregion
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            #region Seed Data
+            modelBuilder.Entity<User>()
+            .HasData(new User
+            {
+                Phone = "09389253640",
+                BirthDate = DateTime.Now,
+                CreateDate = DateTime.Now,
+                FirstName = "reza",
+                Email = "rezakazazy8@yahoo.com",
+                IaActive = true,
+                Id = 1,
+                LastName = "Kazazi",
+                Password = "5600715f42bf51c40dc330d750cd996f58fead4ddea56466ce7498d17801b3a5"
+            });
+            #endregion
+
+            modelBuilder.Entity<User>()
+                .Property(x => x.Phone)
+                .IsRequired()
+                .HasMaxLength(15);
+
+            modelBuilder.Entity<User>()
+                .Property(u => u.Email)
+                .IsRequired()
+                .HasMaxLength(250)
+                .IsUnicode();
+
+            modelBuilder.Entity<User>()
+              .Property(u => u.FirstName)
+              .IsRequired()
+              .HasMaxLength(150)
+              .IsUnicode();
+
+            modelBuilder.Entity<User>()
+              .Property(u => u.LastName)
+              .IsRequired()
+              .HasMaxLength(150)
+              .IsUnicode();
+
+            modelBuilder.Entity<User>()
+              .Property(u => u.Password)
+              .IsRequired()
+              .HasMaxLength(150);
 
         }
 
---
+++ Resume.DataLayer/Migrations/20240720150817_initial_mig.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Resume.DataLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class initial_mig : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(150)", maxLength: 150, nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(150)", maxLength: 150, nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(150)", maxLength: 150, nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(250)", maxLength: 250, nullable: false),
+                    Phone = table.Column<string>(type: "nvarchar(15)", maxLength: 15, nullable: false),
+                    IaActive = table.Column<bool>(type: "bit", nullable: false),
+                    BirthDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CreateDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "BirthDate", "CreateDate", "Email", "FirstName", "IaActive", "LastName", "Password", "Phone" },
+                values: new object[] { 1, new DateTime(2024, 7, 20, 18, 38, 16, 790, DateTimeKind.Local).AddTicks(236), new DateTime(2024, 7, 20, 18, 38, 16, 790, DateTimeKind.Local).AddTicks(246), "rezakazazy8@yahoo.com", "reza", true, "Kazazi", "5600715f42bf51c40dc330d750cd996f58fead4ddea56466ce7498d17801b3a5", "09389253640" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
