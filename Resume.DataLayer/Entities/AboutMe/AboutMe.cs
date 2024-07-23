b175d2aacbe67098c93760b095a60042edc508d6
rezakazazy8 <rezakazazy8@yahoo.com>
2024-07-23 23:36:40 +0330

    Create Table AboutMe

---
+++ Resume.DataLayer/Entities/AboutMe/AboutMe.cs
@@ -0,0 +1,16 @@
+﻿namespace Resume.DataLayer.Entities.AboutMe;
+public class AboutMe:BaseEntity<int>
+{
+    public int UserId { get; set; }
+
+    public string? Bio { get; set; }
+
+    public string? Location { get; set; }
+
+    public string? PictureName { get; set; }
+
+    #region Relations
+
+    public virtual User.User?  User { get; set; }
+    #endregion
+}
---
+++ Resume.DataLayer/Migrations/20240723130708_mig_AboutMe.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Resume.DataLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig_AboutMe : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AboutMe",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<int>(type: "int", nullable: false),
+                    Bio = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Location = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PictureName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CreateDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AboutMe", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_AboutMe_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Skills",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    SkillName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    SkillLevel = table.Column<int>(type: "int", nullable: false),
+                    CreateDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Skills", x => x.Id);
+                });
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "BirthDate", "CreateDate" },
+                values: new object[] { new DateTime(2024, 7, 23, 16, 37, 7, 467, DateTimeKind.Local).AddTicks(4207), new DateTime(2024, 7, 23, 16, 37, 7, 467, DateTimeKind.Local).AddTicks(4229) });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AboutMe_UserId",
+                table: "AboutMe",
+                column: "UserId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AboutMe");
+
+            migrationBuilder.DropTable(
+                name: "Skills");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "BirthDate", "CreateDate" },
+                values: new object[] { new DateTime(2024, 7, 21, 23, 2, 6, 897, DateTimeKind.Local).AddTicks(3495), new DateTime(2024, 7, 21, 23, 2, 6, 897, DateTimeKind.Local).AddTicks(3508) });
+        }
+    }
+}
