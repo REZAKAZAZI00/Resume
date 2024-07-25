057defcde707c88a1a667e3e477db101d2d189c1
rezakazazy8 <rezakazazy8@yahoo.com>
2024-07-25 20:11:32 +0330

    Create ToCustomString for DateTime and DateOnly

---
+++ Resume.Core/Convertors/DateTimeExtensions.cs
@@ -0,0 +1,16 @@
+﻿global using System.Globalization;
+
+namespace Resume.Core.Convertors;
+public static class DateTimeExtensions
+{
+
+    public static string ToCustomString(this DateOnly dateTime)
+    {
+        return dateTime.ToString("MMMM yyyy", CultureInfo.InvariantCulture);
+    } 
+    public static string ToCustomString(this DateTime dateTime)
+    {
+        return dateTime.ToString("MMMM yyyy", CultureInfo.InvariantCulture);
+    }
+}
+
