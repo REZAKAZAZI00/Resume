9cdf6acc9d419caf4987d51035ff4671d946d2b0
rezakazazy8 <rezakazazy8@yahoo.com>
2024-07-27 22:30:16 +0330

    Improvement Code

--- Resume/Controllers/HomeController.cs
+++ Resume/Controllers/HomeController.cs
@@ -1,26 +1,23 @@
-using Resume.Web.Controllers;
-
 namespace Resume.Controllers
 {
     public class HomeController : SiteBaseController
     {
         #region Fields
         private readonly IUserService _userService;
-        private readonly IEducationService _educationService;
-        private readonly ISkillsService _skillsService;
-        private readonly IWorkExperiencesService _workExperiencesService;
         #endregion
 
         #region Constructor
-        public HomeController(IUserService userService, ISkillsService skillsService, IEducationService educationService, IWorkExperiencesService workExperiencesService)
+        public HomeController(IUserService userService)
         {
             _userService = userService;
-            _skillsService = skillsService;
-            _educationService = educationService;
-            _workExperiencesService = workExperiencesService;
+            
         }
         #endregion
 
+
+        #region Actions
+
+        
         [HttpGet("")]
         [HttpGet("/index")]
         [HttpGet("/home/index")]
@@ -28,12 +25,9 @@ namespace Resume.Controllers
         {
             var user= await _userService.GetUserForShowAsync();
             ViewData[Data] = user;
-            ViewData["Skills"]=await _skillsService.GetSkillsInfoShowInHomeAsync();
-            ViewData["Educations"]=await _educationService.GetEducationsAsync();
-            ViewData["WorkEx"]=await _workExperiencesService.GetWorkExperiencesAsync(); 
             return View();
         }
 
-        
+        #endregion
     }
 }
