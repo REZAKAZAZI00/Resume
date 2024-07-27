e221b4b71fd3148353c22913a4b0209ebdd5a862
rezakazazy8 <rezakazazy8@yahoo.com>
2024-07-27 22:40:53 +0330

    Create View Componenet Skills

---
+++ Resume/Components/SkillsViewComponent.cs
@@ -0,0 +1,29 @@
+﻿namespace Resume.Web.Components
+{
+    public class SkillsViewComponent:ViewComponent
+    {
+        #region Fields
+
+        private readonly ISkillsService _skillsService;
+
+
+        #endregion
+
+        #region Constructor
+        public SkillsViewComponent(ISkillsService skillsService)
+        {
+            _skillsService = skillsService;
+        }
+        #endregion
+
+        #region Methods
+
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var skills=await _skillsService.GetSkillsInfoShowInHomeAsync();
+            return View("skills",skills);
+        }
+        #endregion
+    }
+}
