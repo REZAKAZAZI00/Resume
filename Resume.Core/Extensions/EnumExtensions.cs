536f15c6221cff799f231f839c1fb98e591a41ab
rezakazazy8 <rezakazazy8@yahoo.com>
2024-07-28 00:50:02 +0330

    for Upload  and Del eth....

---
+++ Resume.Core/Extensions/EnumExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+
+namespace Resume.Core.Extensions;
+public static class EnumExtensions
+{
+    public static string GetEnumName(this Enum myEnum)
+    {
+        var enumDisplayName = myEnum.GetType()
+            .GetMember(myEnum.ToString())
+            .FirstOrDefault();
+
+        if (enumDisplayName != null)
+            return enumDisplayName.GetCustomAttribute<DisplayAttribute>().GetName();
+
+        return "";
+    }
+}
