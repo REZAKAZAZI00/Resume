63c79a69f2c4bf52a6701fd37ad0573ae86519ba
rezakazazy8 <rezakazazy8@yahoo.com>
2024-07-27 22:41:40 +0330

    Create View Componenet Education

---
+++ Resume/Components/EducationViewComponent.cs
@@ -0,0 +1,31 @@
+﻿using NuGet.Versioning;
+
+namespace Resume.Web.Components
+{
+    public class EducationViewComponent:ViewComponent
+    {
+        #region Fields
+
+        private readonly IEducationService _educationService;
+
+
+        #endregion
+
+        #region Constructor
+        public EducationViewComponent(IEducationService educationService)
+        {
+            _educationService = educationService;
+        }
+        #endregion
+
+
+        #region Methods
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var educations=await _educationService.GetEducationsAsync();
+            return View("Education",educations);
+        }
+        #endregion
+    }
+}
