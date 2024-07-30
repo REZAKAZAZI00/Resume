0cae15f918fb29daf9c6f187ee62b791ea0bf8e5
rezakazazy8 <rezakazazy8@yahoo.com>
2024-07-31 00:40:19 +0330

    add site tools  for root image

---
+++ Resume.Core/Tools/SiteTools.cs
@@ -0,0 +1,23 @@
+﻿namespace Resume.Core.Tools;
+
+public class SiteTools
+{
+
+
+    #region DefaultNames
+
+    public static string DefaultImageName { get; set; }
+
+    #endregion
+
+    #region About me
+
+    public static string AboutMeAvatar { get; set; } = "/img/UserProfile/";
+
+    #endregion
+
+    #region Project
+
+    public static string ImageProject { get; set; } = "/img/Project/";
+    #endregion
+}
