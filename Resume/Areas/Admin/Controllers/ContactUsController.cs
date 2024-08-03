8deb7c7c9491dda91006f033a34cc38f2213ed27
rezakazazy8 <rezakazazy8@yahoo.com>
2024-08-03 23:43:23 +0330

    add contactus

--- Resume/Areas/Admin/Controllers/ContactUsController.cs
+++ Resume/Areas/Admin/Controllers/ContactUsController.cs
@@ -1,5 +1,5 @@
 ﻿namespace Resume.Web.Areas.Admin.Controllers;
-
+[Area("admin")]
 public class ContactUsController : AdminBaseController
 {
     #region Fields
@@ -20,7 +20,7 @@ public class ContactUsController : AdminBaseController
     #region Actions
 
     #region List
-
+    [HttpGet("/admin/contactus")]
     public async Task<ActionResult<OutPutModel<FilterContactUsViewModel>>> List(FilterContactUsViewModel model)
     {
         var result = await _contactUsService.FilterAsync(model);
@@ -31,21 +31,24 @@ public class ContactUsController : AdminBaseController
 
     #region Details
 
+    [HttpGet("/admin/contactus/details/{id}")]
     public async Task<ActionResult<OutPutModel<ContactUsDetailsViewModel>>> Details(int id)
     {
 
         var result=await _contactUsService.GetByIdAsync(id);
 
-        return View(result);
+        return View(result.Result);
     }
 
-
+    [HttpPost("/admin/contactus/details/{id}")]
     public async Task<ActionResult> Details(ContactUsDetailsViewModel model)
     {
         if(!ModelState.IsValid)
             return View(model);
-
-        return View(model);
+        
+        await _contactUsService.AnswerAsync(model);
+       
+        return RedirectToAction("List");
     }
 
     #endregion
