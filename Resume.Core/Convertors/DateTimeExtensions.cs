177a9dd3ceeafd5a93a86c16d3b9191d62c9275b
rezakazazy8 <rezakazazy8@yahoo.com>
2024-08-10 14:26:29 +0330

    set new custom for date time

--- Resume.Core/Convertors/DateTimeExtensions.cs
+++ Resume.Core/Convertors/DateTimeExtensions.cs
@@ -11,6 +11,9 @@ public static class DateTimeExtensions
     public static string ToCustomString(this DateTime dateTime)
     {
         return dateTime.ToString("MMMM yyyy", CultureInfo.InvariantCulture);
+    }public static string ToCustom(this DateTime dateTime)
+    {
+        return dateTime.ToString("dd MMM, yyyy ", CultureInfo.InvariantCulture);
     }
 }
 
