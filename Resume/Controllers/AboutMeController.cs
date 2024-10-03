076d9c2cc0cab2d6aa478184ceba9c48d1d5de54
rezakazazy8 <rezakazazy8@yahoo.com>
2024-10-03 23:49:41 +0330

    about me

---
+++ Resume/Controllers/AboutMeController.cs
@@ -0,0 +1,29 @@
+﻿namespace Resume.Web.Controllers;
+public class AboutMeController :SiteBaseController
+{
+    #region Filelds
+
+    private readonly IUserService _userService;
+	#endregion
+
+	#region Constructor
+	public AboutMeController(IUserService userService)
+	{
+		_userService = userService;
+	}
+	#endregion
+
+
+	#region Actions
+
+	#region AboutMe
+	[HttpGet("about")]
+    public async Task<IActionResult> AboutMe()
+    {
+		ViewData["Data"]=await _userService.GetUserForShowAsync();
+
+		return View();
+    }
+    #endregion
+    #endregion
+}
